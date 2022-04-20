c68da3098d9bc01ad8507d3c9ab6c44b91b536f1
luke2533 <lukefoxwell1@gmail.com>
2022-04-20 15:57:32 +0000

    Heroku debug false

--- app.py
+++ app.py
@@ -564,4 +564,4 @@ def delete_record(record_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=bool(os.environ["DEBUG_MODE"]))
+            debug=False)
